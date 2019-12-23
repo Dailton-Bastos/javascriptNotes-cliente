614255b1e9102b62a86b68dfbcd8f27e51bc6dda
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-23 11:48:48 -0300

    Estrutura do projeto, Bulma e Estilo global do projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./App.scss";
+import { Notification, Section } from "rbx";
 
-const App = () => <h1>JavascriptNotes</h1>;
+const App = () => (
+  <Fragment>
+    <Section>
+      <Notification color="success">JavaScript Notes!</Notification>
+    </Section>
+  </Fragment>
+);
 
 export default App;
