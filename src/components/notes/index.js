b315f565efa2f60bbd98d2b9fa4616b96070aaa0
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-24 22:26:07 -0300

    Criando Sidebar (notas)

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,8 +1,35 @@
 import React, { Fragment, useState } from "react";
 import "../../styles/notes.scss";
+import { push as Menu } from "react-burger-menu";
+import { Column, Button } from "rbx";
 
 const Notes = props => {
-  return <Fragment>Notes...</Fragment>;
+  return (
+    <Fragment>
+      <Column.Group className="notes" id="notes">
+        <Menu
+          pageWrapId={"notes-editor"}
+          isOpen={props.isOpen}
+          onStateChange={state => props.setIsOpen(state.isOpen)}
+          disableAutoFocus
+          outerContainerId={"notes"}
+          customBurgerIcon={false}
+          customCrossIcon={false}
+        >
+          <Column.Group>
+            <Column size={10} offset={1}>
+              Search...
+            </Column>
+          </Column.Group>
+          <p>List...</p>
+        </Menu>
+
+        <Column size={12} className="notes-editor" id="notes-editor">
+          Editor...
+        </Column>
+      </Column.Group>
+    </Fragment>
+  );
 };
 
 export default Notes;
