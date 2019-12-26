be0d9b86ef12e369187f56a39d5f0cc09694a8a2
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-25 21:52:08 -0300

    Listando as notas

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,9 +1,51 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "../../styles/notes.scss";
 import { push as Menu } from "react-burger-menu";
 import { Column, Button } from "rbx";
+import List from "../notes/list";
+import NotesService from "../../services/note";
 
 const Notes = props => {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({
+    title: "",
+    body: "",
+    id: ""
+  });
+
+  // const fetchNotes = async () => {
+  //   const response = await NotesService.index();
+  //   if (response.data.length >= 1) {
+  //     setNotes(response.data.reverse());
+  //     setCurrentNote(response.data[0]);
+  //   }
+  // };
+  async function fetchNotes() {
+    const response = await NotesService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse());
+      setCurrentNote(response.data[0]);
+    }
+  }
+
+  function selectNote(id) {
+    const note = notes.find(note => {
+      return note._id == id;
+    });
+    setCurrentNote(note);
+  }
+
+  // const selectNote = id => {
+  //   const note = notes.find(note => {
+  //     return note._id == id;
+  //   });
+  //   setCurrentNote(note);
+  // };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <Fragment>
       <Column.Group className="notes" id="notes">
@@ -21,7 +63,11 @@ const Notes = props => {
               Search...
             </Column>
           </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note}
+          />
         </Menu>
 
         <Column size={12} className="notes-editor" id="notes-editor">
